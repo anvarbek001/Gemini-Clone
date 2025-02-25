4a009228dddf871c39864dba909815c91c9eb796
anvarbek001 <ergashevanvarbek00700@gmail.com>
2025-02-25 08:48:43 +0500

    Gemini clone

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+/** @format */
+
+import "./Sidebar.css";
+import { assets } from "../../assets/assets.js";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context.jsx";
+
+const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+  const { onSent, prevPromps, setRecentPrompt } = useContext(Context);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {prevPromps.map((item, index) => {
+              return (
+                <div className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.splice(0,18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Setting</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
