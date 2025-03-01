50dcd58a7a2e151c277614a4b28f09b4ef940adc
anvarbek001 <ergashevanvarbek00700@gmail.com>
2025-03-01 10:20:17 +0500

    Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini</p>
+        <p>AI</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
